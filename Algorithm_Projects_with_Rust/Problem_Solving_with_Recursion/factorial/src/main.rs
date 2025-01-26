8c1ce707c3b7d45749a708ff7ece1def68a721d2
Lars Nohle <lars.nohle@gmail.com>
2025-01-26 10:17:33 +0100

    Problem Solving with Recursion: added solution to the factorial exercise.

---
+++ Algorithm_Projects_with_Rust/Problem_Solving_with_Recursion/factorial/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    for n in 0..22 {
+        println!("{}! = {}", n, factorial(n));
+    }
+}
+
+
+
+fn factorial(n: i64) -> i64 {
+    if n <= 1 {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
