5ee1b5df67368a6363b67df9630386b753c40bfb
Lars Nohle <lars.nohle@gmail.com>
2025-03-04 07:32:38 +0100

    Added implementation of the n queens redux problem now using attack counts.

--- Algorithm_Projects_with_Rust/Problem_Solving_with_Recursion/n_queens_redux/src/main.rs
+++ Algorithm_Projects_with_Rust/Problem_Solving_with_Recursion/n_queens_redux/src/main.rs
@@ -14,7 +14,8 @@ fn main() {
     let mut board = [[UNVISITED; NUM_COLS]; NUM_ROWS];
 
     let start = Instant::now();
-    let success = place_queens_4(& mut board, 0);
+    //let success = place_queens_4(& mut board, 0);
+    let success = place_queens_4_with_attack_counts(& mut board);
     let duration = start.elapsed();
 
     println!("Execution time: {:?}", duration);
@@ -39,7 +40,7 @@ fn place_queens_4(board: &mut [[char; NUM_COLS]; NUM_ROWS], col_index: usize) ->
 
     // OK, we haven't places all queens yet.
     // Start with checking if the board is legal at this point.
-    if !board_is_legal(board){
+    if !board_is_legal(board) {
         return false;
     }
 
@@ -63,6 +64,103 @@ fn place_queens_4(board: &mut [[char; NUM_COLS]; NUM_ROWS], col_index: usize) ->
     // We did not find any solution on this path => return false.
     false
 }
+
+fn place_queens_4_with_attack_counts(board: &mut [[char; NUM_COLS]; NUM_ROWS]) -> bool {
+    // Generate attack counts matrix.
+    let mut attack_counts: [[i8; NUM_COLS]; NUM_ROWS] = [[0; NUM_COLS]; NUM_ROWS];
+
+    place_queens_4_with_attack_counts_recurse(board, 0,&mut attack_counts)
+}
+
+fn place_queens_4_with_attack_counts_recurse(board: &mut [[char; NUM_COLS]; NUM_ROWS], col_index: usize, attack_counts: &mut [[i8; NUM_COLS]; NUM_ROWS]) -> bool {
+    // Have we examined all columns? I.e. is col_index >= the number of columns.
+    if col_index >= NUM_COLS {
+        return board_is_a_solution(board, NUM_ROWS as u32);
+    }
+
+    // OK, we're not finished yet.
+    // Board is still legal. Try to place a queen in this column at each row one at a time.
+    for row_index in 0..NUM_ROWS {
+        // Can we place a queen on this square?
+        if attack_counts[row_index][col_index] == 0 {
+            // Place a queen in this row.
+            board[row_index][col_index] = QUEEN;
+
+            // Adjust the attack counts matrix.
+            adjust_attack_counts(attack_counts, row_index, col_index, 1);
+
+            // Perform a recursive call to place a queen in the next (to the right) column.
+            // Did we find a solution in this path?
+            if place_queens_4_with_attack_counts_recurse(board, col_index + 1, attack_counts) {
+                // Yes! Return true to indicate that we did find a solution.
+                return true;
+            }
+
+            // No, apparently placing a queen here did not lead to us finding a solution => remove it
+            // and try placing it in the next row.
+            board[row_index][col_index] = UNVISITED;
+            adjust_attack_counts(attack_counts, row_index, col_index, -1);
+        }
+    }
+
+    // We did not fins any solution when placing a queen in any of the rows in this clumn.
+    // => return false to backtrack.
+    false
+}
+
+fn adjust_attack_counts(attack_counts: &mut [[i8; NUM_COLS]; NUM_ROWS], row_index: usize, col_index: usize, delta: i8) {
+    // Adjust the attack count of the square in question.
+    attack_counts[row_index][col_index] += delta;
+
+    let row_index_as_i8 = row_index as i8;
+    let col_index_as_i8 = col_index as i8;
+
+    // Adjust the attack counts of the squares a queen placed on this square can reach.
+    for i in 1..NUM_ROWS {
+        let i_as_i8 = i as i8;
+
+        // Same row to the right.
+        if row_index + i < NUM_ROWS {
+            attack_counts[row_index + i][col_index] += delta;
+        }
+
+        // Same row to the left.
+        if row_index_as_i8 - i_as_i8 >= 0 {
+            attack_counts[row_index - i][col_index] += delta;
+        }
+
+        // Same col down.
+        if col_index + i < NUM_COLS {
+            attack_counts[row_index][col_index + i] += delta;
+        }
+
+        // Same col up.
+        if col_index_as_i8 - i_as_i8 >= 0 {
+            attack_counts[row_index][col_index - i] += delta;
+        }
+
+        // Diagonal up left.
+        if row_index_as_i8 - i_as_i8 >= 0 && col_index_as_i8- i_as_i8 >= 0 {
+            attack_counts[row_index - i][col_index - i] += delta;
+        }
+
+        // Diagonal up right.
+        if row_index_as_i8 - i_as_i8 >= 0 && col_index + i < NUM_COLS {
+            attack_counts[row_index - i][col_index + i] += delta;
+        }
+
+        // Diagonal down right.
+        if row_index + i < NUM_ROWS && col_index + i < NUM_COLS {
+            attack_counts[row_index + i][col_index + i] += delta;
+        }
+
+        // Diagonal down left.
+        if row_index + i < NUM_ROWS && col_index_as_i8 - i_as_i8 >= 0 {
+            attack_counts[row_index + i][col_index - i] += delta;
+        }
+    }
+}
+
 
 // Return true if the board is legal and a solution.
 fn board_is_a_solution(board: &[[char; NUM_COLS]; NUM_ROWS], num_queens: u32) -> bool {
